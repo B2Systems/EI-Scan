53d86f1b709db3b67ca55c4a7c4b810b02bac735
Christian Braybrooke <chris@purplemountmedia.com>
2018-07-17 23:01:28 +0100

    Add dist

---
+++ dist/precache-manifest.cfcdf43eab5acaad86fd9398a8d27ef9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "3f244fad78b33677726c0b5d3fcae40a",
+    "url": "/media/scan.3f244fad.mp3"
+  },
+  {
+    "revision": "07a9490299b99375751a",
+    "url": "/js/chunk-vendors.07a94902.js"
+  },
+  {
+    "revision": "1f5ae051a627eaa5fb20",
+    "url": "/js/barcode-scanner-component.1f5ae051.js"
+  },
+  {
+    "revision": "baac5aab4131be5b3fc1",
+    "url": "/js/app.baac5aab.js"
+  },
+  {
+    "revision": "53dac9c2baa56ead94ff8da6837795f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "1f5ae051a627eaa5fb20",
+    "url": "/css/barcode-scanner-component.824c00e2.css"
+  },
+  {
+    "revision": "baac5aab4131be5b3fc1",
+    "url": "/css/app.95c4dd33.css"
+  }
+];
